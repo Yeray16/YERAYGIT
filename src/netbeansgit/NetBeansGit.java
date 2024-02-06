c1e8b077fc151a04fe4f31de33303f03c94c71dc
Yeray16 <yeraynp16@gmail.com>
2024-02-06 16:19:16 +0000

    version 1.0 prueba

---
+++ src/netbeansgit/NetBeansGit.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package netbeansgit;
+
+/**
+ *
+ * @author Daw1
+ */
+public class NetBeansGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+    
+}
